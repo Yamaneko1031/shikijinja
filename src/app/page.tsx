e4a11c1a7bd25bc2bd29d57b6e1117d9bdc8f590
Nekoyama <candoanything1031@gmail.com>
2025-04-06 15:42:51 +0900

    初期コミット

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div>式岐神社</div>
+      <div>心得</div>
+      <div>絵馬</div>
+      <div>おみくじ</div>
+      <div>神様紹介</div>
+      <div>拝殿</div>
+    </div>
+  );
+}
