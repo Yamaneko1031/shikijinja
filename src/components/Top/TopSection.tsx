5af72741b830d7fe26736c576b23f51d952040eb
Nekoyama <candoanything1031@gmail.com>
2025-04-28 22:55:21 +0900

    ソース整理

--- src/components/Top/TopSection.tsx
+++ src/components/Top/TopSection.tsx
@@ -17,8 +17,6 @@ const TopSection = ({ isActive, isNeighbor }: Props) => {
   const toriiScale = useTransform(scrollY, [0, 400], [1.2, 3.5]);
   const toriiOpacity = useTransform(scrollY, [100, 500], [1, 0]);
   const titleOpacity = useTransform(scrollY, [150, 400], [1, 0]);
-  // const whiteOverlayOpacity = useTransform(scrollY, [200, 650, 900], [0, 1, 0]);
-  const arrowOpacity = useTransform(scrollY, [0, 200], [1, 0]);
 
   return (
     <>
@@ -64,50 +62,14 @@ const TopSection = ({ isActive, isNeighbor }: Props) => {
           delayPerChar={0.1}
         />
       </motion.div>
-      {/* <motion.h1
-        style={{ opacity: titleOpacity }}
-        className="absolute top-[40%] left-1/2 -translate-x-1/2 text-white text-4xl sm:text-6xl font-bold drop-shadow-[0_0_5px_rgba(0,0,0,1)] z-30 pointer-events-none"
-      >
-        式岐神社
-      </motion.h1>
 
-      <motion.p
-        style={{ opacity: titleOpacity }}
-        className="absolute top-[48%] left-1/2 -translate-x-1/2 z-30 text-white text-center text-lg drop-shadow"
-      >
-        IT業界で働く人、IT業界を志す人に
-        <br />
-        ご利益をもたらすと言い伝えられている神社
-      </motion.p> */}
-      <motion.div
-        className="fixed z-1000 left-1/2 bottom-[8vh] -translate-x-1/2 pointer-events-none text-white"
-        style={{ opacity: arrowOpacity }}
-        animate={{ y: [0, -10, 0] }}
-        transition={{ duration: 1.5, repeat: Infinity, ease: 'easeInOut' }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-          className="w-8 h-8"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
-        </svg>
-      </motion.div>
+      {/* 落ち葉 */}
       {isActive && (
         <div className="leaf">
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            {Array.from({ length: 9 }).map((_, index) => (
+              <li key={index}></li>
+            ))}
           </ul>
         </div>
       )}
