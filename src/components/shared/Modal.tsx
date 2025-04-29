7f6822c9c413474bca3cc25d59b69f083c9b09d1
Nekoyama <candoanything1031@gmail.com>
2025-04-30 00:31:53 +0900

    仮対応

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -23,6 +23,12 @@ const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
     //   document.body.style.right = '';
     //   window.scrollTo(0, scrollY);
     // };
+
+    // 仮処理 iosでは上手くいかない
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
   }, [isOpen]);
   return isOpen ? (
     <div className="fixed top-[50lvh] left-1/2 translate-x-[-50%] translate-y-[-50%] z-100 bg-black/80 rounded-lg p-4 text-white">
