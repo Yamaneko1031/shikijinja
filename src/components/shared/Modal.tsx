aafda1a23f6661366ed0908e8ba2ed2f458b7dff
Nekoyama <candoanything1031@gmail.com>
2025-04-30 00:24:34 +0900

    テスト

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -8,21 +8,21 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ isOpen, children }) => {
   useLayoutEffect(() => {
     if (!isOpen) return;
-    const scrollY = window.scrollY;
-    document.documentElement.style.overscrollBehaviorY = 'none';
-    document.body.style.position = 'fixed';
-    document.body.style.top = `-${scrollY}px`;
-    document.body.style.left = '0';
-    document.body.style.right = '0';
+    // const scrollY = window.scrollY;
+    // document.documentElement.style.overscrollBehaviorY = 'none';
+    // document.body.style.position = 'fixed';
+    // document.body.style.top = `-${scrollY}px`;
+    // document.body.style.left = '0';
+    // document.body.style.right = '0';
 
-    return () => {
-      document.documentElement.style.overscrollBehaviorY = '';
-      document.body.style.position = '';
-      document.body.style.top = '';
-      document.body.style.left = '';
-      document.body.style.right = '';
-      window.scrollTo(0, scrollY);
-    };
+    // return () => {
+    //   document.documentElement.style.overscrollBehaviorY = '';
+    //   document.body.style.position = '';
+    //   document.body.style.top = '';
+    //   document.body.style.left = '';
+    //   document.body.style.right = '';
+    //   window.scrollTo(0, scrollY);
+    // };
   }, [isOpen]);
   return isOpen ? (
     <div className="fixed top-[50lvh] left-1/2 translate-x-[-50%] translate-y-[-50%] z-100 bg-black/80 rounded-lg p-4 text-white">
