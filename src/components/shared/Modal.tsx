f40a9375fdd76d00182738bd54d65db4b6bfa0cd
Nekoyama <candoanything1031@gmail.com>
2025-04-29 21:28:16 +0900

    テスト

--- src/components/shared/Modal.tsx
+++ src/components/shared/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 
 interface ModalProps {
   children: React.ReactNode;
@@ -6,14 +6,29 @@ interface ModalProps {
 
 const Modal: React.FC<ModalProps> = ({ children }) => {
   // スクロール禁止
-  useEffect(() => {
-    const body = document.querySelector('body');
-    if (!body) return;
-    if (document.scrollingElement) {
-      body.classList.add('overflow-hidden');
-    }
+  // useEffect(() => {
+  //   const body = document.querySelector('body');
+  //   if (!body) return;
+  //   if (document.scrollingElement) {
+  //     body.classList.add('overflow-hidden');
+  //   }
+  //   return () => {
+  //     body.classList.remove('overflow-hidden');
+  //   };
+  // }, []);
+  useLayoutEffect(() => {
+    const scrollY = window.scrollY;
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollY}px`;
+    document.body.style.left = '0';
+    document.body.style.right = '0';
+
     return () => {
-      body.classList.remove('overflow-hidden');
+      document.body.style.position = '';
+      document.body.style.top = '';
+      document.body.style.left = '';
+      document.body.style.right = '';
+      window.scrollTo(0, scrollY);
     };
   }, []);
   return <>{children}</>;
