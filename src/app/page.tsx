2c669cca448b01476e62d65067c787117583df44
Nekoyama <candoanything1031@gmail.com>
2025-05-02 22:39:07 +0900

    テスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
 'use client';
 
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, createContext, Dispatch, SetStateAction } from 'react';
 import { BackgroundManager, BackgroundManagerHandle } from '@/components/shared/BackgroundManager';
 import { Section, sections } from '@/config/sections';
 import DebugLogDialog from '@/components/shared/DebugLogDialog';
 import SectionOverlayText from '@/components/shared/SctionOverlayText';
 
+type DialogCountContextType = [number, Dispatch<SetStateAction<number>>];
+export const DialogCountContext = createContext<DialogCountContextType>([0, () => {}]);
+
 export default function App() {
   console.log('App');
+  const dialogState = useState<number>(0);
   const [state, setState] = useState({ activeId: sections[0].id });
+  const [dialogCount] = dialogState;
   const sectionRefs = useRef<Record<string, HTMLElement | null>>({});
   const bgManagerRef = useRef<BackgroundManagerHandle>(null);
   const currentSectionRef = useRef<Section>(sections[0]);
   const allUrls = sections.map(({ bgUrl }) => bgUrl);
   const activeIndex = sections.findIndex((s) => s.id === state.activeId);
+  const dialogCountRef = useRef(dialogCount);
+
+  useEffect(() => {
+    dialogCountRef.current = dialogCount;
+  }, [dialogCount]);
 
   useEffect(() => {
     const checkScroll = () => {
+      if (dialogCountRef.current > 0) {
+        // console.log('checkScroll return');
+        requestAnimationFrame(checkScroll);
+        return;
+      }
       const centerY = window.innerHeight / 2;
 
       const nextSection = sections.find(({ id }) => {
@@ -54,37 +69,39 @@ export default function App() {
   }, []);
 
   return (
-    <main className="relative z-10 overflow-x-hidden">
-      <BackgroundManager
-        ref={bgManagerRef}
-        initialUrl={sections[0].bgUrl}
-        allUrls={allUrls}
-        scrollEffect={sections[0].scrollEffect}
-      />
+    <DialogCountContext.Provider value={dialogState}>
+      <main className="relative z-10 overflow-x-hidden">
+        <BackgroundManager
+          ref={bgManagerRef}
+          initialUrl={sections[0].bgUrl}
+          allUrls={allUrls}
+          scrollEffect={sections[0].scrollEffect}
+        />
 
-      {sections.map(({ id, component: SectionComponent }, idx) => {
-        const isActive = id === state.activeId;
-        const isNeighbor = Math.abs(idx - activeIndex) === 1;
-        return (
-          <section
-            key={id}
-            ref={(el) => {
-              sectionRefs.current[id] = el;
-            }}
-            className={`${sections[idx].sectionClass}`}
-          >
-            {(isActive || isNeighbor) && SectionComponent && (
-              <SectionComponent isActive={isActive} isNeighbor={isNeighbor} />
-            )}
-          </section>
-        );
-      })}
+        {sections.map(({ id, component: SectionComponent }, idx) => {
+          const isActive = id === state.activeId;
+          const isNeighbor = Math.abs(idx - activeIndex) === 1;
+          return (
+            <section
+              key={id}
+              ref={(el) => {
+                sectionRefs.current[id] = el;
+              }}
+              className={`${sections[idx].sectionClass}`}
+            >
+              {(isActive || isNeighbor) && SectionComponent && (
+                <SectionComponent isActive={isActive} isNeighbor={isNeighbor} />
+              )}
+            </section>
+          );
+        })}
 
-      <SectionOverlayText text={currentSectionRef.current.name} />
+        <SectionOverlayText text={currentSectionRef.current.name} />
 
-      <DebugLogDialog />
+        <DebugLogDialog />
 
-      <div className="overlay-gradient" />
-    </main>
+        <div className="overlay-gradient" />
+      </main>
+    </DialogCountContext.Provider>
   );
 }
