70512dce456e352ac79c6b0a9715b33f34045c38
Nekoyama <candoanything1031@gmail.com>
2025-05-17 13:50:48 +0900

    ヘッダー配置調整

--- src/components/_shared/Header.tsx
+++ src/components/_shared/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-[50px] bg-white z-1">
       <div className="flex items-center justify-center">
-        <div className="left-0 w-[280px] h-[50px] flex items-center justify-center">
+        <div className="left-0 min-w-[200px] h-[50px] flex items-center justify-center">
           {/* <Image src="/images/icon/icon_logo.webp" alt="式岐神社" width={40} height={40} /> */}
           <h1 className="text-2xl text-black font-bold">式岐神社</h1>
           <span className="ml-2 text-xs text-gray-600">SHIKI JINJA</span>
