fa17da4ea22e69d1d92b1e39c3a7a2fa4fe7b0b2
Nekoyama <candoanything1031@gmail.com>
2025-05-17 12:09:17 +0900

    読込み画面調整

--- src/components/Omikuji/OmikujiLoding.tsx
+++ src/components/Omikuji/OmikujiLoding.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const OmikujiLoding: React.FC = () => {
   console.log('OmikujiLoding');
   return (
-    <div className="relative min-w-[340px] w-[400px] h-[300px] border-2 border-white/50 rounded-lg bg-white/10">
-      <div className="absolute top-2 left-1/2 -translate-x-1/2 w-[240px] text-left text-white text-2xl font-bold omikuji-lot">
+    <div className="relative mx-8 min-w-[340px] w-[440px] h-[300px] bg-[url('/images/bg_hude/bg_washi.webp')] bg-[length:100%_100%]">
+      <div className="absolute top-2 left-1/2 -translate-x-1/2 w-[240px] text-left text-black text-2xl font-bold omikuji-lot">
         おみくじ抽選中
       </div>
-      <div className="absolute bottom-0 left-0 omikuji-inori" aria-hidden="true"></div>
+      <div className="absolute bottom-2 left-2 omikuji-inori" aria-hidden="true"></div>
       <Image
         src="/images/omikuji/omikuji_box.webp"
         alt="omikuji_box"
