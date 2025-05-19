f28b54ee7d51bd65f9743c504c502700309bb739
Nekoyama <candoanything1031@gmail.com>
2025-05-19 18:21:53 +0900

    メニューボタン追加

--- src/components/_shared/Header.tsx
+++ src/components/_shared/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import Image from 'next/image';
+import Image from 'next/image';
+
 const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 w-full h-[50px] bg-white z-1">
@@ -8,6 +9,18 @@ const Header: React.FC = () => {
           {/* <Image src="/images/icon/icon_logo.webp" alt="式岐神社" width={40} height={40} /> */}
           <h1 className="text-2xl text-black font-bold">式岐神社</h1>
           <span className="ml-2 text-xs text-gray-600">SHIKI JINJA</span>
+          <div className="absolute right-2 h-full flex items-center">
+            <button
+              className="p-2 hover:bg-gray-100 rounded-md"
+              //   aria-label="メニュー"
+              //   onClick={() => {
+              //     // TODO: メニューを開く処理を実装
+              //     console.log('メニューを開く');
+              //   }}
+            >
+              <Image src="/images/icon/icon_menu.webp" alt="メニュー" width={40} height={40} />
+            </button>
+          </div>
         </div>
         <div className="w-full h-full"></div>
       </div>
