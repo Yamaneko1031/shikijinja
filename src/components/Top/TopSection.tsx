6859afabf396c8f266530b90c7441a1861011030
Nekoyama <candoanything1031@gmail.com>
2025-06-15 03:45:31 +0900

    すずめ実装

--- src/components/Top/TopSection.tsx
+++ src/components/Top/TopSection.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { motion, useTransform } from 'framer-motion';
 import Image from 'next/image';
 import TextReveal from '../_shared/TextReveal';
@@ -13,6 +13,55 @@ const TopSection = (props: SectionProps) => {
   const toriiScale = useTransform(props.scrollY, [0, 400], [1, 3.5]);
   const toriiOpacity = useTransform(props.scrollY, [100, 500], [1, 0]);
   const titleOpacity = useTransform(props.scrollY, [150, 400], [1, 0]);
+  const suzumeScale = useTransform(props.scrollY, [0, 1200], [1, 1.2]);
+
+  const [suzumeAnimationClass1, setSuzumeAnimationClass1] = useState('hidden');
+  const [suzumeAnimationClass2, setSuzumeAnimationClass2] = useState('hidden');
+  const inAnimationSetting = useRef(false);
+  const inAnimationFinished = useRef(false);
+
+  useEffect(() => {
+    const timeouts: NodeJS.Timeout[] = [];
+
+    if (props.isActive) {
+      timeouts[0] = setInterval(() => {
+        if (inAnimationSetting.current === false) {
+          if (props.scrollY.get() < 10) {
+            inAnimationSetting.current = true;
+            timeouts[1] = setTimeout(() => {
+              setSuzumeAnimationClass1('animate-suzume-in');
+            }, 200);
+
+            timeouts[2] = setTimeout(() => {
+              setSuzumeAnimationClass2('animate-suzume-in');
+            }, 500);
+
+            timeouts[3] = setTimeout(() => {
+              setSuzumeAnimationClass1('');
+              setSuzumeAnimationClass2('');
+              inAnimationFinished.current = true;
+            }, 2500);
+          }
+        }
+        if (inAnimationFinished.current && props.scrollY.get() > 10) {
+          inAnimationFinished.current = false;
+          setSuzumeAnimationClass1('animate-suzume-out');
+          timeouts[4] = setTimeout(() => {
+            setSuzumeAnimationClass2('animate-suzume-out');
+          }, 200);
+        }
+      }, 60);
+    } else {
+      inAnimationSetting.current = false;
+      setSuzumeAnimationClass1('hidden');
+      setSuzumeAnimationClass2('hidden');
+      timeouts.forEach((timeout) => clearTimeout(timeout));
+    }
+
+    return () => {
+      timeouts.forEach((timeout) => clearTimeout(timeout));
+    };
+  }, [props.scrollY, props.isActive]);
 
   return (
     <div className="pointer-events-none">
@@ -25,8 +74,10 @@ const TopSection = (props: SectionProps) => {
             left: '50%',
             scale: toriiScale,
             opacity: toriiOpacity,
+            // width: '80vw',
+            // height: 'calc(80vw * 1)',
             width: '80vw',
-            height: 'calc(80vw * 1)',
+            height: '50lvh',
             maxHeight: '80vh',
             transformOrigin: '50% 70%',
           }}
@@ -58,6 +109,33 @@ const TopSection = (props: SectionProps) => {
 
       {/* 落ち葉 */}
       {props.isActive && <ReafParticles />}
+
+      {props.isActive && (
+        <motion.div
+          className="fixed inset-0 w-[100%] h-[100lvh] z-31"
+          style={{ scale: suzumeScale }}
+          // style={{
+          //   position: 'absolute',
+          //   inset: 0,
+          //   width: '100%',
+          //   height: '100lvh',
+          //   objectFit: 'cover',
+          //   objectPosition: 'center',
+          // opacity: url === activeUrl ? 1 : url === prevUrl ? 0 : 0,
+          // zIndex: url === activeUrl ? 1 : url === prevUrl ? 0 : -1,
+          // transition: 'opacity 1.2s ease',
+          // display: 'block',
+          // transform: `scale(${url === activeUrl ? scale : url === prevUrl ? prevScale : 1}) translateZ(0.01px)`,
+          // }}
+        >
+          <div
+            className={`absolute left-1/2 top-1/2 -translate-x-[100%] translate-y-[100%] w-[12lvh] h-[12lvh] suzume ${suzumeAnimationClass1}`}
+          ></div>
+          <div
+            className={`absolute left-1/2 top-1/2 -translate-x-[150%] translate-y-[150%] w-[11lvh] h-[11lvh] suzume ${suzumeAnimationClass2}`}
+          ></div>
+        </motion.div>
+      )}
     </div>
   );
 };
