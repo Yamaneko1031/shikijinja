e0572cdd3aab0fc19f441cda94a1fea1b62d6a55
Nekoyama <candoanything1031@gmail.com>
2025-07-08 16:40:33 +0900

    ヘルプ、ポリシー更新

--- src/components/Header/HelpToku.tsx
+++ src/components/Header/HelpToku.tsx
@@ -12,22 +12,49 @@ export default function HelpToku({ onClose }: Props) {
     <div className="w-full h-full flex flex-col items-center text-black">
       <div className="min-h-[3rem] flex justify-center items-center">徳コインについて</div>
       <div className="max-h-[60vh] w-full overflow-scroll m-1 p-2 border-2 border-gray-100">
-        徳コインとは式岐神社で利用できる通貨です。
-        <br />
-        おみくじやお守りの購入、賽銭を投げる際に必要になります。
-        <br />
-        特定の行動を行う事で貯まっていきます。
-        <br />
-        <br />
-        ■徳の貯まる行動
-        <ul>
-          <li>・鳥居をくぐる（1日1回）</li>
-          <li>・絵馬を書く（1日1回）</li>
-          <li>・絵馬をタップする（1日5回）</li>
-          <li>・なで猫をなでる（1日1回）</li>
-          <li>・アカウント登録をする（初回のみ）</li>
-          <li>・神様紹介を見る（初回のみ）</li>
-          <li>・十戒を見る（初回のみ）</li>
+        <ul className="help-ul">
+          <li>
+            <h3>徳コインとは</h3>
+            <p>
+              徳コインとは式岐神社で利用できる通貨です。
+              <br />
+              おみくじやお守りの購入、賽銭を投げる際に必要になります。
+              <br />
+              特定の行動を行う事で貯まっていきます。
+            </p>
+          </li>
+          <li>
+            <h3>徳の貯まる行動</h3>
+            <ul>
+              <li>
+                <p>鳥居をくぐる（1日1回）</p>
+              </li>
+              <li>
+                <p>絵馬を書く（1日1回）</p>
+              </li>
+              <li>
+                <p>絵馬をタップする（1日5回）</p>
+              </li>
+              <li>
+                <p>なで猫をなでる（1日1回）</p>
+              </li>
+              <li>
+                <p>アカウント登録をする（初回のみ）</p>
+              </li>
+              <li>
+                <p>神様紹介を見る（初回のみ）</p>
+              </li>
+              <li>
+                <p>十戒を見る（初回のみ）</p>
+              </li>
+              <li>
+                <p>おまもりの結果をシェアする（初回のみ）</p>
+              </li>
+              <li>
+                <p>おみくじの結果をシェアする（初回のみ）</p>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
       <div className="min-h-[3rem] flex justify-center items-center">
