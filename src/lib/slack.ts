ea386f9b910a073db7510576964ea2ba37f9c455
Nekoyama <candoanything1031@gmail.com>
2025-07-11 20:01:52 +0900

    slack処理修正

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -1,7 +1,9 @@
 export const postSlackError = async (message: string) => {
-  await fetch('/api/slack', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ message }),
-  });
+  if (process.env.NODE_ENV !== 'production') {
+    await fetch('/api/slack', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message }),
+    });
+  }
 };
